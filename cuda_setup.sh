afd441b7068af1c2ea85a618ee5e8531753fcebe
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2025-12-30 10:30:18 +1000

    Create windows setup script and modify cuda setup script for Unix

--- cuda_setup.sh
+++ cuda_setup.sh
@@ -1,3 +1,5 @@
+python -m venv venv
+
 source venv/bin/activate
 pip install -r cuda-requirements.txt # If your cuda version is different than 12.x, modify the gpu-requirements file to reflect that
 
