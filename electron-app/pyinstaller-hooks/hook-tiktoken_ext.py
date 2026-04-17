91f779f27aca4a03e6110d385d8e56bb1350aaef
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-04-17 08:24:32 -0400

    chore: change all files to cpp ending, cause I hate myself

--- electron-app/pyinstaller-hooks/hook-tiktoken_ext.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import collect_submodules, collect_data_files
-
-hiddenimports = collect_submodules('tiktoken_ext')
-datas = collect_data_files('tiktoken_ext')
--- src/embeddings/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Embedding layer module for transformer models.
-
-This module provides token and positional embeddings for transformer architectures.
-"""
-from .embeddings import EmbeddingLayer
